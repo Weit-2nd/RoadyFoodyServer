bfcf3876c4796ce443c5edf0bcad0006c44e6eb1
konempty <1117plus@gmail.com>
2024-05-26 21:45:10 +0900

    init commit

---
+++ src/main/kotlin/kr/weit/roadyfoody/RoadyFoodyApplication.kt
@@ -0,0 +1,11 @@
+package kr.weit.roadyfoody
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class RoadyFoodyApplication
+
+fun main(args: Array<String>) {
+    runApplication<RoadyFoodyApplication>(*args)
+}
---
+++ src/test/kotlin/kr/weit/roadyfoody/RoadyFoodyApplicationTests.kt
@@ -0,0 +1,12 @@
+package kr.weit.roadyfoody
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class RoadyFoodyApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+}
