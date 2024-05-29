8d6d232eb5573da40c7e64f4960c11fb81cb8882
konempty <jameswebb@naver.com>
2024-05-29 23:42:36 +0900

    ROFO-14 불필요한 어노테이션 삭제

--- src/main/kotlin/kr/weit/roadyfoody/RoadyFoodyApplication.kt
+++ src/main/kotlin/kr/weit/roadyfoody/RoadyFoodyApplication.kt
@@ -1,10 +1,8 @@
 package kr.weit.roadyfoody
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
-import org.springframework.boot.context.properties.ConfigurationPropertiesScan
 import org.springframework.boot.runApplication
 
-@ConfigurationPropertiesScan
 @SpringBootApplication
 class RoadyFoodyApplication
 
