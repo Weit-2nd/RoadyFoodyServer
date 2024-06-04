817507b262168f2276cfe1f55975c8af724dc93f
unagi-zoso <unagi.zoso@gmail.com>
2024-06-05 03:22:52 +0900

    ROFO-20 NICKNAME_REGEX_DESC 수정

--- src/main/kotlin/kr/weit/roadyfoody/support/regex/ValidationRegex.kt
+++ src/main/kotlin/kr/weit/roadyfoody/support/regex/ValidationRegex.kt
@@ -4,4 +4,4 @@ const val NICKNAME_MIN_LENGTH = 6
 const val NICKNAME_MAX_LENGTH = 16
 const val NICKNAME_REGEX_STR = "^[A-Za-z0-9가-힣]{$NICKNAME_MIN_LENGTH,$NICKNAME_MAX_LENGTH}$"
 val NICKNAME_REGEX = Regex(NICKNAME_REGEX_STR)
-const val NICKNAME_REGEX_DESC = "닉네임은 6자 이상 16자 이하의 한글, 영문, 숫자 여야 합니다."
+const val NICKNAME_REGEX_DESC = "닉네임은 $NICKNAME_MIN_LENGTH 자 이상 $NICKNAME_MAX_LENGTH 자 이하의 한글, 영문, 숫자 여야 합니다."
