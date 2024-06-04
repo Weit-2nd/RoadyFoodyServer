92aa942726d99cb261f7078cdd6be1da4f55bd4c
unagi-zoso <unagi.zoso@gmail.com>
2024-06-05 00:06:32 +0900

    ROFO-20 닉네임 유효성 검사 요소 세분화 (글자 수 최소, 최대 길이 추가)

--- src/main/kotlin/kr/weit/roadyfoody/support/regex/ValidationRegex.kt
+++ src/main/kotlin/kr/weit/roadyfoody/support/regex/ValidationRegex.kt
@@ -1,5 +1,7 @@
 package kr.weit.roadyfoody.support.regex
 
-const val NICKNAME_REGEX_STR = "^[A-Za-z0-9가-힣]{6,16}$"
+const val NICKNAME_MIN_LENGTH = 6
+const val NICKNAME_MAX_LENGTH = 16
+const val NICKNAME_REGEX_STR = "^[A-Za-z0-9가-힣]{$NICKNAME_MIN_LENGTH,$NICKNAME_MAX_LENGTH}$"
 val NICKNAME_REGEX = Regex(NICKNAME_REGEX_STR)
 const val NICKNAME_REGEX_DESC = "닉네임은 6자 이상 16자 이하의 한글, 영문, 숫자 여야 합니다."
