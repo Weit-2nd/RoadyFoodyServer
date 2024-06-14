aa032aa20f53149c23aa829db5f9a3ce6b9769b5
unagi-zoso <unagi.zoso@gmail.com>
2024-06-15 02:35:57 +0900

    ROFO-35 TermQueryService 및 테스트 코드 작성
    `calculateEachTypeSizes` 메소드는 입력 받은 약관 콜렉션을 사용해 전체 약관 사이즈, 필수 약관 사이즈, 선택 약관 사이즈 셋을 반환하는 기능입니다.
    반환타입으로 이 의미를 담을 수 있는 클래스를 새로 만들까 생각도 하였지만 해당 메소드의 사용처가 이후에도 나올 것 같지 않아. `Triple` 로 뒀습니다.
    
    메소드 자체에도 세 가지의 사이즈 반환이라는 역할이 있는데 이걸 하나의 책임으로 봐도 될지 아니면 따로 메소드로 다 분리할 지 고민하였지만
    이 부분도 이 메소드의 이후 사용처를 고려하였을 때 이 상태로 관리하여도 괜찮을 것 같다 생각하여 유지하였습니다.

---
+++ src/test/kotlin/kr/weit/roadyfoody/term/service/TermQueryServiceTest.kt
@@ -0,0 +1,104 @@
+package kr.weit.roadyfoody.term.service
+
+import io.kotest.core.spec.style.BehaviorSpec
+import io.kotest.matchers.equals.shouldBeEqual
+import io.mockk.every
+import io.mockk.mockk
+import io.mockk.spyk
+import io.mockk.verify
+import kr.weit.roadyfoody.term.domain.Term
+import kr.weit.roadyfoody.term.exception.TermNotFoundException
+import kr.weit.roadyfoody.term.exception.TermNotFoundException.Companion.termNotFoundMessage
+import kr.weit.roadyfoody.term.fixture.TEST_EACH_TYPE_TERMS_SIZES
+import kr.weit.roadyfoody.term.fixture.TEST_NONEXISTENT_TERM_ID
+import kr.weit.roadyfoody.term.fixture.TEST_OPTIONAL_TERMS_SIZE
+import kr.weit.roadyfoody.term.fixture.TEST_REQUIRED_TERMS_SIZE
+import kr.weit.roadyfoody.term.fixture.TEST_REQUIRED_TERM_1_ID
+import kr.weit.roadyfoody.term.fixture.createTestDetailedTermsResponse
+import kr.weit.roadyfoody.term.fixture.createTestRequiredTerm1
+import kr.weit.roadyfoody.term.fixture.createTestSummaryTermsResponse
+import kr.weit.roadyfoody.term.fixture.createTestTerms
+import kr.weit.roadyfoody.term.repository.TermRepository
+import kr.weit.roadyfoody.term.repository.getByTermId
+import kr.weit.roadyfoody.term.service.dto.DetailedTermResponse
+import org.junit.jupiter.api.assertThrows
+
+class TermQueryServiceTest :
+    BehaviorSpec({
+        val termRepository = mockk<TermRepository>()
+        val termQueryService = spyk(TermQueryService(termRepository), recordPrivateCalls = true)
+
+        given("getAllSummaryTerms 테스트") {
+            `when`("필수 약관이 $TEST_REQUIRED_TERMS_SIZE 개 , 선택 약관이 $TEST_OPTIONAL_TERMS_SIZE 개 존재할 시") {
+                every { termRepository.findAll() } returns createTestTerms()
+                every { termQueryService["calculateEachTypeSizes"](any<List<Term>>()) } returns TEST_EACH_TYPE_TERMS_SIZES
+                then("SummaryTermsResponse 를 반환한다.") {
+                    termQueryService.getAllSummaryTerms() shouldBeEqual createTestSummaryTermsResponse()
+                    verify { termRepository.findAll() }
+                    verify { termQueryService["calculateEachTypeSizes"](any<List<Term>>()) }
+                }
+            }
+
+            `when`("약관이 존재하지 않을 시") {
+                every { termRepository.findAll() } returns emptyList()
+                every { termQueryService["calculateEachTypeSizes"](any<List<Term>>()) } returns Triple(0, 0, 0)
+                then("내용이 0인 SummaryTermsResponse 를 반환한다.") {
+                    termQueryService.getAllSummaryTerms().also {
+                        it.allTermsSize shouldBeEqual 0
+                        it.requiredTermsSize shouldBeEqual 0
+                        it.optionalTermsSize shouldBeEqual 0
+                        it.terms shouldBeEqual emptyList()
+                    }
+                    verify { termRepository.findAll() }
+                    verify { termQueryService["calculateEachTypeSizes"](any<List<Term>>()) }
+                }
+            }
+        }
+
+        given("getAllDetailedTerms 테스트") {
+            `when`("필수 약관이 $TEST_REQUIRED_TERMS_SIZE 개 , 선택 약관이 $TEST_OPTIONAL_TERMS_SIZE 개 존재할 시") {
+                every { termRepository.findAll() } returns createTestTerms()
+                every { termQueryService["calculateEachTypeSizes"](any<List<Term>>()) } returns TEST_EACH_TYPE_TERMS_SIZES
+                then("DetailedTermsResponse 를 반환한다.") {
+                    termQueryService.getAllDetailedTerms() shouldBeEqual createTestDetailedTermsResponse()
+                    verify { termRepository.findAll() }
+                    verify { termQueryService["calculateEachTypeSizes"](any<List<Term>>()) }
+                }
+            }
+
+            `when`("약관이 존재하지 않을 시") {
+                every { termRepository.findAll() } returns emptyList()
+                every { termQueryService["calculateEachTypeSizes"](any<List<Term>>()) } returns Triple(0, 0, 0)
+                then("내용이 0인 DetailedTermsResponse 를 반환한다.") {
+                    termQueryService.getAllDetailedTerms().also {
+                        it.allTermsSize shouldBeEqual 0
+                        it.requiredTermsSize shouldBeEqual 0
+                        it.optionalTermsSize shouldBeEqual 0
+                        it.terms shouldBeEqual emptyList()
+                    }
+                    verify { termRepository.findAll() }
+                    verify { termQueryService["calculateEachTypeSizes"](any<List<Term>>()) }
+                }
+            }
+        }
+
+        given("getDetailedTerm 테스트") {
+            `when`("termId 가 $TEST_REQUIRED_TERM_1_ID 일 시") {
+                every { termRepository.getByTermId(TEST_REQUIRED_TERM_1_ID) } returns createTestRequiredTerm1()
+                then("DetailedTermResponse 를 반환한다.") {
+                    termQueryService.getDetailedTerm(TEST_REQUIRED_TERM_1_ID) shouldBeEqual
+                        DetailedTermResponse.from(createTestRequiredTerm1())
+                    verify { termRepository.getByTermId(TEST_REQUIRED_TERM_1_ID) }
+                }
+            }
+
+            `when`("termId 가 존재하지 않을 시") {
+                every { termRepository.getByTermId(TEST_NONEXISTENT_TERM_ID) } throws
+                    TermNotFoundException(termNotFoundMessage(TEST_NONEXISTENT_TERM_ID))
+                then("TermNotFoundException 를 던진다.") {
+                    assertThrows<TermNotFoundException> { termQueryService.getDetailedTerm(TEST_NONEXISTENT_TERM_ID) }
+                    verify { termRepository.getByTermId(TEST_NONEXISTENT_TERM_ID) }
+                }
+            }
+        }
+    })
