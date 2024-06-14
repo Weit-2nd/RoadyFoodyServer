56413181abe05e7315d2164c0d84580751243e8e
unagi-zoso <unagi.zoso@gmail.com>
2024-06-15 02:40:41 +0900

    ROFO-35 TermCommandService 및 테스트 코드 작성
    `checkRequiredTermsOrThrow` 메소드가 조회의 성향이 강한지 데이터 조작의 성향이 강한지 고민을 했습니다.
    저는 조회를 **조회한 무언가를 반환받길 기대하는 행위** 라 생각합니다.
    이 메소드는 반환하기 보다는 사용자가 전달한 데이터를 조작해 예외 여부를 검사하는 목적이 강해
    `조회` 의 성향과는 거리가 있다 판단해 `TermCommandService` 에 작성하였습니다.

---
+++ src/test/kotlin/kr/weit/roadyfoody/term/service/TermCommandServiceTest.kt
@@ -0,0 +1,59 @@
+package kr.weit.roadyfoody.term.service
+
+import io.kotest.assertions.throwables.shouldNotThrow
+import io.kotest.assertions.throwables.shouldThrow
+import io.kotest.core.spec.style.BehaviorSpec
+import io.mockk.every
+import io.mockk.mockk
+import io.mockk.verify
+import kr.weit.roadyfoody.term.fixture.TEST_REQUIRED_TERMS_SIZE
+import kr.weit.roadyfoody.term.fixture.createTestNotEnoughRequiredTermIds
+import kr.weit.roadyfoody.term.fixture.createTestRequiredTermIds
+import kr.weit.roadyfoody.term.fixture.createTestTermIds
+import kr.weit.roadyfoody.term.repository.TermRepository
+import kr.weit.roadyfoody.useragreedterm.exception.RequiredTermNotAgreedException
+
+class TermCommandServiceTest :
+    BehaviorSpec({
+        val termRepository = mockk<TermRepository>()
+        val termCommandService = TermCommandService(termRepository)
+
+        given("checkRequiredTermsOrThrow 테스트") {
+            every { termRepository.findAllIdsByRequiredFlagIsTrue() } returns createTestRequiredTermIds()
+            `when`("모든 필수 약관 ($TEST_REQUIRED_TERMS_SIZE 개) 과 모든 선택 약관을 전달받으면") {
+                then("RequiredTermNotAgreedException 을 던지지 않는다.") {
+                    shouldNotThrow<RequiredTermNotAgreedException> {
+                        termCommandService.checkRequiredTermsOrThrow(createTestTermIds())
+                    }
+                    verify { termRepository.findAllIdsByRequiredFlagIsTrue() }
+                }
+            }
+
+            `when`("모든 필수 약관 ($TEST_REQUIRED_TERMS_SIZE 개) 만 전달받으면") {
+                then("RequiredTermNotAgreedException 을 던지지 않는다.") {
+                    shouldNotThrow<RequiredTermNotAgreedException> {
+                        termCommandService.checkRequiredTermsOrThrow(createTestTermIds())
+                    }
+                    verify { termRepository.findAllIdsByRequiredFlagIsTrue() }
+                }
+            }
+
+            `when`("일부 필수 약관을 전달 받지 못하면") {
+                then("RequiredTermNotAgreedException 을 던진다.") {
+                    shouldThrow<RequiredTermNotAgreedException> {
+                        termCommandService.checkRequiredTermsOrThrow(createTestNotEnoughRequiredTermIds())
+                    }
+                    verify { termRepository.findAllIdsByRequiredFlagIsTrue() }
+                }
+            }
+
+            `when`("모든 필수 약관을 전달 받지 못하면") {
+                then("RequiredTermNotAgreedException 을 던진다.") {
+                    shouldThrow<RequiredTermNotAgreedException> {
+                        termCommandService.checkRequiredTermsOrThrow(emptyList())
+                    }
+                    verify { termRepository.findAllIdsByRequiredFlagIsTrue() }
+                }
+            }
+        }
+    })
