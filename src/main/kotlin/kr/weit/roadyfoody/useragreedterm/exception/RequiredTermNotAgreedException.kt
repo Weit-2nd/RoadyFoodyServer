997a5804517f201c8af2adb2a049c578f7812a60
unagi-zoso <unagi.zoso@gmail.com>
2024-06-15 02:28:17 +0900

    ROFO-35 필수약관 미동의 시 커스텀 예외 작성

---
+++ src/main/kotlin/kr/weit/roadyfoody/useragreedterm/exception/RequiredTermNotAgreedException.kt
@@ -0,0 +1,17 @@
+package kr.weit.roadyfoody.useragreedterm.exception
+
+import kr.weit.roadyfoody.common.exception.BaseException
+import kr.weit.roadyfoody.common.exception.ErrorCode
+
+class RequiredTermNotAgreedException(
+    message: String,
+) : BaseException(ErrorCode.INVALID_REQUEST, message) {
+    companion object {
+        private const val REQUIRED_TERM_NOT_AGREED_MESSAGE_PREFIX = "약관 ID:"
+        private const val REQUIRED_TERM_NOT_AGREED_MESSAGE_SUFFIX = "필수 약관에 동의하지 않았습니다."
+
+        @JvmStatic
+        fun requiredTermNotAgreedMessage(termIds: Iterable<Long>) =
+            "$REQUIRED_TERM_NOT_AGREED_MESSAGE_PREFIX ${termIds.joinToString()} $REQUIRED_TERM_NOT_AGREED_MESSAGE_SUFFIX"
+    }
+}
