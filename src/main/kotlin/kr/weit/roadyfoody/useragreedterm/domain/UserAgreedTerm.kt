a4340c552b0e1fa05086a4e27bf84ebaf49da066
unagi-zoso <unagi.zoso@gmail.com>
2024-06-15 02:58:04 +0900

    ROFO-35 유저동의약관 Entity 클래스 및 DDL 작성
    조회에 대한 기능은 계획에 없어 인덱스 설정 같은 부분은 추후에 일어날 수 있습니다.

---
+++ src/main/kotlin/kr/weit/roadyfoody/useragreedterm/domain/UserAgreedTerm.kt
@@ -0,0 +1,35 @@
+package kr.weit.roadyfoody.useragreedterm.domain
+
+import jakarta.persistence.Entity
+import jakarta.persistence.GeneratedValue
+import jakarta.persistence.GenerationType
+import jakarta.persistence.Id
+import jakarta.persistence.JoinColumn
+import jakarta.persistence.ManyToOne
+import jakarta.persistence.SequenceGenerator
+import jakarta.persistence.Table
+import jakarta.persistence.UniqueConstraint
+import kr.weit.roadyfoody.common.domain.BaseTimeEntity
+import kr.weit.roadyfoody.term.domain.Term
+import kr.weit.roadyfoody.user.domain.User
+
+@Entity
+@Table(
+    name = "user_agreed_terms",
+    uniqueConstraints = [
+        UniqueConstraint(columnNames = ["user_id", "term_id"]),
+
+    ],
+)
+@SequenceGenerator(name = "USER_AGREED_TERMS_SEQ_GENERATOR", sequenceName = "user_agreed_terms_seq", initialValue = 1, allocationSize = 1)
+class UserAgreedTerm(
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "USER_AGREED_TERMS_SEQ_GENERATOR")
+    val id: Long = 0L,
+    @ManyToOne
+    @JoinColumn(name = "user_id", nullable = false)
+    val user: User,
+    @ManyToOne
+    @JoinColumn(name = "term_id", nullable = false)
+    val term: Term,
+) : BaseTimeEntity()
