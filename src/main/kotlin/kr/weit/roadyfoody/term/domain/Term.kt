2d254ecc9fd11be098a791287f1df9522238b328
unagi-zoso <unagi.zoso@gmail.com>
2024-06-15 02:01:34 +0900

    ROFO-35 약관 Entity 클래스 및 DDL 작성

---
+++ src/main/kotlin/kr/weit/roadyfoody/term/domain/Term.kt
@@ -0,0 +1,25 @@
+package kr.weit.roadyfoody.term.domain
+
+import jakarta.persistence.Column
+import jakarta.persistence.Entity
+import jakarta.persistence.GeneratedValue
+import jakarta.persistence.GenerationType
+import jakarta.persistence.Id
+import jakarta.persistence.SequenceGenerator
+import jakarta.persistence.Table
+import kr.weit.roadyfoody.common.domain.BaseModifiableEntity
+
+@Entity
+@Table(name = "terms")
+@SequenceGenerator(name = "TERMS_SEQ_GENERATOR", sequenceName = "terms_seq", initialValue = 1, allocationSize = 1)
+class Term(
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "TERMS_SEQ_GENERATOR")
+    val id: Long = 0L,
+    @Column(nullable = false, length = 90, unique = true)
+    val title: String,
+    @Column(columnDefinition = "CLOB", nullable = false)
+    val content: String,
+    @Column(name = "required_flag")
+    val requiredFlag: Boolean,
+) : BaseModifiableEntity()
