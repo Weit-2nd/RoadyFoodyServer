cd13b60628ab5a25266dfbcf635cdccf46774da1
unagi-zoso <unagi.zoso@gmail.com>
2024-06-16 16:53:14 +0900

    ROFO-35 NO_SUCH_ELEMENT 에러코드 대체 커스텀 예외 에러코드 추가

--- src/main/kotlin/kr/weit/roadyfoody/term/exception/TermNotFoundException.kt
+++ src/main/kotlin/kr/weit/roadyfoody/term/exception/TermNotFoundException.kt
@@ -5,7 +5,7 @@ import kr.weit.roadyfoody.common.exception.ErrorCode
 
 class TermNotFoundException(
     message: String,
-) : BaseException(ErrorCode.NO_SUCH_ELEMENT, message) {
+) : BaseException(ErrorCode.NOT_FOUND_TERM, message) {
     companion object {
         private const val TERM_NOT_FOUND_MESSAGE_PREFIX = "약관 ID:"
         private const val TERM_NOT_FOUND_MESSAGE_SUFFIX = "약관을 찾을 수 없습니다."
