4f67f0bde399d3c234c235170c7aca12b78d9c68
unagi-zoso <unagi.zoso@gmail.com>
2024-06-17 14:55:44 +0900

    ROFO-35 storeUserAgreedTerms 메소드 패러미터 타입 변경 List -> Set

--- src/test/kotlin/kr/weit/roadyfoody/useragreedterm/service/UserAgreedTermCommandServiceTest.kt
+++ src/test/kotlin/kr/weit/roadyfoody/useragreedterm/service/UserAgreedTermCommandServiceTest.kt
@@ -5,12 +5,13 @@ import io.mockk.every
 import io.mockk.mockk
 import io.mockk.verify
 import kr.weit.roadyfoody.term.fixture.TEST_NONEXISTENT_TERM_ID
-import kr.weit.roadyfoody.term.fixture.createTestTermIds
+import kr.weit.roadyfoody.term.fixture.createTestTermIdSet
 import kr.weit.roadyfoody.term.fixture.createTestTerms
 import kr.weit.roadyfoody.term.repository.TermRepository
-import kr.weit.roadyfoody.user.fixture.createTestUser1
+import kr.weit.roadyfoody.user.fixture.TEST_USER_ID
+import kr.weit.roadyfoody.user.fixture.createTestUser
 import kr.weit.roadyfoody.useragreedterm.domain.UserAgreedTerm
-import kr.weit.roadyfoody.useragreedterm.fixture.createTestUser1AgreedTerms
+import kr.weit.roadyfoody.useragreedterm.fixture.createTestUserAgreedTerms
 import kr.weit.roadyfoody.useragreedterm.repository.UserAgreedTermRepository
 
 class UserAgreedTermCommandServiceTest :
@@ -21,31 +22,34 @@ class UserAgreedTermCommandServiceTest :
 
         given("storeUserAgreedTerms 테스트") {
             `when`("단일 User 와 하나 이상의 termId 가 주어질 시") {
-                every { termRepository.findAllByIdIn(any<List<Long>>()) } returns createTestTerms()
-                every { userAgreedTermRepository.saveAll(any<List<UserAgreedTerm>>()) } returns createTestUser1AgreedTerms()
+                every { termRepository.findAllByIdIn(any<Set<Long>>()) } returns createTestTerms()
+                every { userAgreedTermRepository.saveAll(any<List<UserAgreedTerm>>()) } returns
+                    createTestUserAgreedTerms(
+                        TEST_USER_ID,
+                    )
                 then("해당 UserAgreedTerm 들을 저장한다.") {
-                    userAgreedTermCommandService.storeUserAgreedTerms(createTestUser1(), createTestTermIds())
-                    verify { termRepository.findAllByIdIn(any<List<Long>>()) }
+                    userAgreedTermCommandService.storeUserAgreedTerms(createTestUser(TEST_USER_ID), createTestTermIdSet())
+                    verify { termRepository.findAllByIdIn(any<Set<Long>>()) }
                     verify { userAgreedTermRepository.saveAll(any<List<UserAgreedTerm>>()) }
                 }
             }
 
             `when`("단일 User 와 존재하지 않는 termId 가 주어질 시") {
-                every { termRepository.findAllByIdIn(any<List<Long>>()) } returns emptyList()
+                every { termRepository.findAllByIdIn(any<Set<Long>>()) } returns emptyList()
                 every { userAgreedTermRepository.saveAll(any<List<UserAgreedTerm>>()) } returns emptyList()
                 then("해당 UserAgreedTerm 들을 저장하지 않는다.") {
-                    userAgreedTermCommandService.storeUserAgreedTerms(createTestUser1(), listOf(TEST_NONEXISTENT_TERM_ID))
-                    verify { termRepository.findAllByIdIn(any<List<Long>>()) }
+                    userAgreedTermCommandService.storeUserAgreedTerms(createTestUser(TEST_USER_ID), setOf(TEST_NONEXISTENT_TERM_ID))
+                    verify { termRepository.findAllByIdIn(any<Set<Long>>()) }
                     verify { userAgreedTermRepository.saveAll(any<List<UserAgreedTerm>>()) }
                 }
             }
 
             `when`("단일 User 와 빈 termId 가 주어질 시") {
-                every { termRepository.findAllByIdIn(emptyList()) } returns emptyList()
+                every { termRepository.findAllByIdIn(emptySet()) } returns emptyList()
                 every { userAgreedTermRepository.saveAll(emptyList()) } returns emptyList()
                 then("해당 UserAgreedTerm 들을 저장하지 않는다.") {
-                    userAgreedTermCommandService.storeUserAgreedTerms(createTestUser1(), emptyList())
-                    verify { termRepository.findAllByIdIn(emptyList()) }
+                    userAgreedTermCommandService.storeUserAgreedTerms(createTestUser(TEST_USER_ID), emptySet())
+                    verify { termRepository.findAllByIdIn(emptySet()) }
                     verify { userAgreedTermRepository.saveAll(emptyList()) }
                 }
             }
