c8d4bb60fe6411fe2ed53fdf28bd5a277c07f7e3
unagi-zoso <unagi.zoso@gmail.com>
2024-06-17 14:42:21 +0900

    ROFO-35 Term Entity 변수, 컬렴명 변경 requiredFlag -> required

--- src/main/kotlin/kr/weit/roadyfoody/term/repository/TermRepository.kt
+++ src/main/kotlin/kr/weit/roadyfoody/term/repository/TermRepository.kt
@@ -2,16 +2,15 @@ package kr.weit.roadyfoody.term.repository
 
 import kr.weit.roadyfoody.term.domain.Term
 import kr.weit.roadyfoody.term.exception.TermNotFoundException
-import kr.weit.roadyfoody.term.exception.TermNotFoundException.Companion.getTermNotFoundMessage
 import org.springframework.data.jpa.repository.JpaRepository
 import org.springframework.data.jpa.repository.Query
 import org.springframework.data.repository.findByIdOrNull
 
-fun TermRepository.getByTermId(termId: Long): Term = findByIdOrNull(termId) ?: throw TermNotFoundException(getTermNotFoundMessage(termId))
+fun TermRepository.getByTermId(termId: Long): Term = findByIdOrNull(termId) ?: throw TermNotFoundException(termId)
 
 interface TermRepository : JpaRepository<Term, Long> {
-    fun findAllByIdIn(termIds: List<Long>): List<Term>
+    fun findAllByIdIn(termIdSet: Set<Long>): List<Term>
 
-    @Query("SELECT t.id FROM Term t WHERE t.requiredFlag = true")
-    fun findAllIdsByRequiredFlagIsTrue(): List<Long>
+    @Query("SELECT t.id FROM Term t WHERE t.required = true")
+    fun findAllIdsByRequiredIsTrue(): Set<Long>
 }
