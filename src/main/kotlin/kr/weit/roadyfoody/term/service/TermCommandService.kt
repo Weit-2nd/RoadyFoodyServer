2b2788dfbcf576a7cb5471db01e0dca64d2867a3
unagi-zoso <unagi.zoso@gmail.com>
2024-06-17 14:48:26 +0900

    ROFO-35 checkRequiredTermsOrThrow 메소드 패러미터 타입 Set 으로 변경
    현재 계획 중인 기능 상 List 로 다뤄질 일이 크게 없어 외부에서 입력 받는 시점부터(Request Dto) Set 으로 받을 예정입니다.
    따라서 굳이 내부에서 Set 으로 변경할 이유가 사라졌습니다.

--- src/main/kotlin/kr/weit/roadyfoody/term/service/TermCommandService.kt
+++ src/main/kotlin/kr/weit/roadyfoody/term/service/TermCommandService.kt
@@ -2,21 +2,18 @@ package kr.weit.roadyfoody.term.service
 
 import kr.weit.roadyfoody.term.repository.TermRepository
 import kr.weit.roadyfoody.useragreedterm.exception.RequiredTermNotAgreedException
-import kr.weit.roadyfoody.useragreedterm.exception.RequiredTermNotAgreedException.Companion.getRequiredTermNotAgreedMessage
 import org.springframework.stereotype.Service
 
 @Service
 class TermCommandService(
     private val termRepository: TermRepository,
 ) {
-    fun checkRequiredTermsOrThrow(agreedTermIds: List<Long>) {
-        val requiredTermIds = termRepository.findAllIdsByRequiredFlagIsTrue()
-        val agreedTermIdSet = agreedTermIds.toSet()
-
-        val notAgreedTermIdSet = requiredTermIds.subtract(agreedTermIdSet)
+    fun checkRequiredTermsOrThrow(agreedTermIdSet: Set<Long>) {
+        val requiredTermIdSet = termRepository.findAllIdsByRequiredIsTrue()
+        val notAgreedTermIdSet = requiredTermIdSet.subtract(agreedTermIdSet)
 
         if (notAgreedTermIdSet.isNotEmpty()) {
-            throw RequiredTermNotAgreedException(getRequiredTermNotAgreedMessage(notAgreedTermIdSet))
+            throw RequiredTermNotAgreedException(notAgreedTermIdSet)
         }
     }
 }
