d5e171acf57407c4c1118a75544e9d4346000a91
unagi-zoso <unagi.zoso@gmail.com>
2024-06-18 01:45:57 +0900

    ROFO-35 간략한 약관 response 약관 사이즈 구하는 로직 추가
    굳이 서비스에서 약관 사이즈들을 구하는 로직을 두지 않고 response dto의 생성자에서 구하도록 변경합니다.

--- src/test/kotlin/kr/weit/roadyfoody/term/fixture/TermDtoFixture.kt
+++ src/test/kotlin/kr/weit/roadyfoody/term/fixture/TermDtoFixture.kt
@@ -3,24 +3,9 @@ package kr.weit.roadyfoody.term.fixture
 import kr.weit.roadyfoody.term.service.dto.DetailedTermResponse
 import kr.weit.roadyfoody.term.service.dto.SummaryTermsResponse
 
-fun createTestSummaryTermsResponse() =
-    SummaryTermsResponse.from(
-        TEST_TERMS_SIZE,
-        TEST_REQUIRED_TERMS_SIZE,
-        TEST_OPTIONAL_TERMS_SIZE,
-        createTestTerms(),
-    )
+fun createTestSummaryTermsResponse() = SummaryTermsResponse.from(createTestTerms())
 
-fun createTestDetailedTermResponse(id: Long) =
-    DetailedTermResponse.from(
-        createTestRequiredTerm(id),
-    )
+fun createTestDetailedTermResponse(id: Long) = DetailedTermResponse.from(createTestRequiredTerm(id))
 
 // fail case
-fun createTestZerosSummaryTermsResponse() =
-    SummaryTermsResponse.from(
-        0,
-        0,
-        0,
-        emptyList(),
-    )
+fun createTestZerosSummaryTermsResponse() = SummaryTermsResponse.from(emptyList())
