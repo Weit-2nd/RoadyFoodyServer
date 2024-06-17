1511ac9d5c981a77f3dbddd11e705b1d5994a5f6
unagi-zoso <unagi.zoso@gmail.com>
2024-06-18 01:43:22 +0900

    ROFO-35 간략한 약관 조회 API 경로 변경
    `summary` 는 없어도 오해할만한 다른 API 가 없어 생략해도 좋을 것 같다 판단했습니다.

--- src/main/kotlin/kr/weit/roadyfoody/term/presentation/api/TermController.kt
+++ src/main/kotlin/kr/weit/roadyfoody/term/presentation/api/TermController.kt
@@ -14,7 +14,7 @@ import org.springframework.web.bind.annotation.RestController
 class TermController(
     private val termQueryService: TermQueryService,
 ) : TermControllerSpec {
-    @GetMapping("/summary")
+    @GetMapping
     override fun getAllSummaryTerms(): SummaryTermsResponse = termQueryService.getAllSummaryTerms()
 
     @GetMapping("/{termId}")
