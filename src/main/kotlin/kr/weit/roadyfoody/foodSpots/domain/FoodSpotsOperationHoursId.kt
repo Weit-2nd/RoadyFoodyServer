02e1a51a658beaa161d3078633d74e8864eb68d6
Chae <oumuamua121@gmail.com>
2024-06-19 19:38:12 +0900

    ROFO-47 음식점 & 리뷰 엔티티 작성

---
+++ src/main/kotlin/kr/weit/roadyfoody/foodSpots/domain/FoodSpotsOperationHoursId.kt
@@ -0,0 +1,8 @@
+package kr.weit.roadyfoody.foodSpots.domain
+
+import java.io.Serializable
+
+data class FoodSpotsOperationHoursId(
+    val foodSpots: FoodSpots,
+    val dayOfWeek: DayOfWeek,
+) : Serializable
