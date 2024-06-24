c93e9fce52dff90d8bd42411fa35efa3c921c40f
unagi-zoso <unagi.zoso@gmail.com>
2024-06-24 22:58:25 +0900

    ROFO-42 Test Fixture 피드백 반영

--- src/test/kotlin/kr/weit/roadyfoody/auth/fixture/AuthFixture.kt
+++ src/test/kotlin/kr/weit/roadyfoody/auth/fixture/AuthFixture.kt
@@ -3,11 +3,12 @@ package kr.weit.roadyfoody.auth.fixture
 import kr.weit.roadyfoody.auth.domain.SocialAccessToken
 import kr.weit.roadyfoody.auth.dto.KakaoUserResponse
 import kr.weit.roadyfoody.auth.dto.SignUpRequest
+import kr.weit.roadyfoody.support.utils.ImageFormat
+import kr.weit.roadyfoody.support.utils.generateImageBytes
 import kr.weit.roadyfoody.term.fixture.createTestRequiredTermIdSet
 import kr.weit.roadyfoody.user.fixture.TEST_SOCIAL_ID
 import kr.weit.roadyfoody.user.fixture.TEST_SOCIAL_LOGIN_TYPE
 import kr.weit.roadyfoody.user.fixture.TEST_USER_NICKNAME
-import org.springframework.core.io.ClassPathResource
 import org.springframework.mock.web.MockMultipartFile
 import org.springframework.web.multipart.MultipartFile
 import java.time.LocalDateTime
@@ -23,7 +24,7 @@ fun createTestKakaoUserResponse(): KakaoUserResponse =
 
 fun createTestSignUpRequest(
     termIdSet: Set<Long> = createTestRequiredTermIdSet(),
-    profileImage: MultipartFile? = TEST_WEBP_IMAGE,
+    profileImage: MultipartFile? = createTestImageFile(ImageFormat.WEBP),
 ): SignUpRequest =
     SignUpRequest(
         TEST_USER_NICKNAME,
@@ -32,19 +33,12 @@ fun createTestSignUpRequest(
         TEST_SOCIAL_LOGIN_TYPE,
     )
 
-val TEST_WEBP_IMAGE_BYTES = ClassPathResource("image/webp_test_img.webp").inputStream.readBytes()
-val TEST_WEBP_IMAGE = MockMultipartFile("mockImage", "mockImage.webp", "image/webp", TEST_WEBP_IMAGE_BYTES)
-const val TEST_WEBP_EXTENSION = ".webp"
+fun createTestImageFile(format: ImageFormat): MultipartFile =
+    MockMultipartFile(
+        "mockImage${format.values}",
+        "mockImage${format.values}.$format.value",
+        "image/$format.value",
+        generateImageBytes(format),
+    )
 
 const val TEST_BEARER_TOKEN = "Bearer test-token"
-
-// fail case
-
-// 확장자만 webp 이고 내용은 gif
-val TEST_FAKE_WEBP_IMAGE_BYTES = ClassPathResource("image/fake_webp_test_img.webp").inputStream.readBytes()
-
-val TEST_JPEG_IMAGE_BYTES = ClassPathResource("image/jpeg_test_img.jpeg").inputStream.readBytes()
-
-val TEST_PNG_IMAGE_BYTES = ClassPathResource("image/png_test_img.png").inputStream.readBytes()
-
-val TEST_GIF_IMAGE_BYTES = ClassPathResource("image/gif_test_img.gif").inputStream.readBytes()
