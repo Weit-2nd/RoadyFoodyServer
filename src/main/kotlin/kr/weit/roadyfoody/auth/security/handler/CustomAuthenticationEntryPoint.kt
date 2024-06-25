fec6ba07638588265bf53c6382851af13eafaa0d
jifrozen0110 <answldjs1836@gmail.com>
2024-06-26 01:31:12 +0900

    ROFO-46 머지 에러 수정

--- src/main/kotlin/kr/weit/roadyfoody/auth/security/handler/CustomAuthenticationEntryPoint.kt
+++ src/main/kotlin/kr/weit/roadyfoody/auth/security/handler/CustomAuthenticationEntryPoint.kt
@@ -5,10 +5,6 @@ import jakarta.servlet.http.HttpServletRequest
 import jakarta.servlet.http.HttpServletResponse
 import kr.weit.roadyfoody.common.exception.ErrorCode
 import kr.weit.roadyfoody.common.exception.ErrorResponse
-<<<<<<< HEAD:src/main/kotlin/kr/weit/roadyfoody/user/security/handler/CustomAuthenticationEntryPoint.kt
-=======
-import kr.weit.roadyfoody.global.utils.ObjectMapperProvider
->>>>>>> 9845b5c0a8559be075801c18b0eeb7283f1d8779:src/main/kotlin/kr/weit/roadyfoody/auth/security/handler/CustomAuthenticationEntryPoint.kt
 import org.springframework.security.core.AuthenticationException
 import org.springframework.security.web.AuthenticationEntryPoint
 import org.springframework.stereotype.Component
