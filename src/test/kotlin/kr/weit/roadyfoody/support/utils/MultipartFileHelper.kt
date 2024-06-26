32e8049505a70da08be31ebdfa15410ba7119a04
unagi-zoso <unagi.zoso@gmail.com>
2024-06-26 10:12:23 +0900

    ROFO-42 createTestImageFile 메소드 위치 이동 -> MultipartFileHelper
    테스트 시 여러 기능에서 이미지를 만들 경우가 많아보여
    좀 더 공용으로 사용할 수 있는 위치에 뒀습니다.
    
    근데 MultipartFileHelper 라는 이름이 적합한지는 조금 의문이 듭니다.
    공용 Fixture 의 이름을 어떻게 지을까 고민하다 정하긴 했지만 더 좋은 이름이 있다면 바꾸는 것도 괜찮아보입니다.

--- src/test/kotlin/kr/weit/roadyfoody/support/utils/MultipartFileHelper.kt
+++ src/test/kotlin/kr/weit/roadyfoody/support/utils/MultipartFileHelper.kt
@@ -9,3 +9,10 @@ fun createMultipartFile(
     contentStream: InputStream,
     contentType: String = MediaType.APPLICATION_JSON_VALUE,
 ): MockMultipartFile = MockMultipartFile(name, name, contentType, contentStream)
+
+const val TEST_IMAGE_FILE_NAME = "testImage"
+
+fun createTestImageFile(
+    format: ImageFormat,
+    name: String = TEST_IMAGE_FILE_NAME,
+): MockMultipartFile = createMultipartFile(name, generateImageBytes(format).inputStream(), "image/${format.first()}")
