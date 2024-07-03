7364c206a6bddf9244f72103e480eb91a5b0ec13
jifrozen0110 <answldjs1836@gmail.com>
2024-07-04 00:39:38 +0900

    ROFO-64 작업 반영
    * ROFO-64 작업을 sandbox 를 base로 두어 작업을 하였기 때문에 해당 작업을 develop에 반영하기 위함

--- src/main/kotlin/kr/weit/roadyfoody/search/address/presentation/spec/AddressSearchControllerSpec.kt
+++ src/main/kotlin/kr/weit/roadyfoody/search/address/presentation/spec/AddressSearchControllerSpec.kt
@@ -3,14 +3,17 @@ package kr.weit.roadyfoody.search.address.presentation.spec
 import io.swagger.v3.oas.annotations.Operation
 import io.swagger.v3.oas.annotations.Parameter
 import io.swagger.v3.oas.annotations.media.Content
-import io.swagger.v3.oas.annotations.media.ExampleObject
 import io.swagger.v3.oas.annotations.media.Schema
 import io.swagger.v3.oas.annotations.responses.ApiResponse
 import io.swagger.v3.oas.annotations.tags.Tag
-import kr.weit.roadyfoody.common.exception.ErrorResponse
+import jakarta.validation.constraints.Positive
+import jakarta.validation.constraints.Size
+import kr.weit.roadyfoody.common.exception.ErrorCode
+import kr.weit.roadyfoody.global.swagger.ApiErrorCodeExamples
 import kr.weit.roadyfoody.global.swagger.v1.SwaggerTag
 import kr.weit.roadyfoody.search.address.application.dto.AddressSearchResponse
 import kr.weit.roadyfoody.search.address.application.dto.AddressSearchResponses
+import org.springframework.web.bind.annotation.RequestParam
 
 @Tag(name = SwaggerTag.SEARCH)
 interface AddressSearchControllerSpec {
@@ -30,30 +33,20 @@ interface AddressSearchControllerSpec {
                     ),
                 ],
             ),
-            ApiResponse(
-                responseCode = "500",
-                description = "외부 API 호출 문제",
-                content = [
-                    Content(
-                        mediaType = "application/json",
-                        schema = Schema(implementation = ErrorResponse::class),
-                        examples = [
-                            ExampleObject(
-                                value = """
-                        {
-                            "code": -11000,
-                            "errorMessage": "외부 API 호출 중 에러 발생"
-                        }
-                        """,
-                            ),
-                        ],
-                    ),
-                ],
-            ),
+        ],
+    )
+    @ApiErrorCodeExamples(
+        [
+            ErrorCode.REST_CLIENT_ERROR,
+            ErrorCode.SIZE_NON_POSITIVE,
+            ErrorCode.SEARCH_KEYWORD_LENGTH,
         ],
     )
     fun searchAddress(
+        @RequestParam
+        @Positive(message = "조회할 개수는 양수여야 합니다.")
         numOfRows: Int,
-        keyword: String,
+        @Size(min = 2, max = 60, message = "검색어는 2자 이상 60자 이하로 입력해주세요.")
+        @RequestParam keyword: String,
     ): AddressSearchResponses
 }
--- src/main/kotlin/kr/weit/roadyfoody/search/tourism/presentation/spec/TourismControllerSpec.kt
+++ src/main/kotlin/kr/weit/roadyfoody/search/tourism/presentation/spec/TourismControllerSpec.kt
@@ -3,13 +3,16 @@ package kr.weit.roadyfoody.search.tourism.presentation.spec
 import io.swagger.v3.oas.annotations.Operation
 import io.swagger.v3.oas.annotations.Parameter
 import io.swagger.v3.oas.annotations.media.Content
-import io.swagger.v3.oas.annotations.media.ExampleObject
 import io.swagger.v3.oas.annotations.media.Schema
 import io.swagger.v3.oas.annotations.responses.ApiResponse
 import io.swagger.v3.oas.annotations.tags.Tag
-import kr.weit.roadyfoody.common.exception.ErrorResponse
+import jakarta.validation.constraints.Positive
+import jakarta.validation.constraints.Size
+import kr.weit.roadyfoody.common.exception.ErrorCode
+import kr.weit.roadyfoody.global.swagger.ApiErrorCodeExamples
 import kr.weit.roadyfoody.global.swagger.v1.SwaggerTag
 import kr.weit.roadyfoody.search.tourism.dto.SearchResponses
+import org.springframework.web.bind.annotation.RequestParam
 
 @Tag(name = SwaggerTag.SEARCH)
 interface TourismControllerSpec {
@@ -29,30 +32,20 @@ interface TourismControllerSpec {
                     ),
                 ],
             ),
-            ApiResponse(
-                responseCode = "500",
-                description = "외부 API 호출 문제",
-                content = [
-                    Content(
-                        mediaType = "application/json",
-                        schema = Schema(implementation = ErrorResponse::class),
-                        examples = [
-                            ExampleObject(
-                                value = """
-                        {
-                            "code": -11000,
-                            "errorMessage": "외부 API 호출 중 에러 발생"
-                        }
-                        """,
-                            ),
-                        ],
-                    ),
-                ],
-            ),
+        ],
+    )
+    @ApiErrorCodeExamples(
+        [
+            ErrorCode.REST_CLIENT_ERROR,
+            ErrorCode.SIZE_NON_POSITIVE,
+            ErrorCode.SEARCH_KEYWORD_LENGTH,
         ],
     )
     fun searchTourismKeyword(
+        @RequestParam
+        @Positive(message = "조회할 개수는 양수여야 합니다.")
         numOfRows: Int,
-        keyword: String,
+        @Size(min = 2, max = 60, message = "검색어는 2자 이상 60자 이하로 입력해주세요.")
+        @RequestParam keyword: String,
     ): SearchResponses
 }
