60223f2e2dc6cd8cc823b3da5b428ef0a07bc7df
jifrozen0110 <answldjs1836@gmail.com>
2024-07-10 11:27:02 +0900

    ROFO-63 검색어 제한 2자 이상 -> 1자 이상

--- src/main/kotlin/kr/weit/roadyfoody/common/exception/ErrorCode.kt
+++ src/main/kotlin/kr/weit/roadyfoody/common/exception/ErrorCode.kt
@@ -2,7 +2,11 @@ package kr.weit.roadyfoody.common.exception
 
 import org.springframework.http.HttpStatus
 
-enum class ErrorCode(val httpStatus: HttpStatus, val code: Int, val errorMessage: String) {
+enum class ErrorCode(
+    val httpStatus: HttpStatus,
+    val code: Int,
+    val errorMessage: String,
+) {
     INVALID_REQUEST(HttpStatus.BAD_REQUEST, -10000, "Invalid request"),
     UNAUTHORIZED(HttpStatus.UNAUTHORIZED, -10001, "Unauthorized"),
     FORBIDDEN(HttpStatus.FORBIDDEN, -10002, "Forbidden"),
@@ -24,6 +28,6 @@ enum class ErrorCode(val httpStatus: HttpStatus, val code: Int, val errorMessage
     // Search API error 11000대
     REST_CLIENT_ERROR(HttpStatus.INTERNAL_SERVER_ERROR, -11000, "외부 API 호출 중 에러 발생"),
     RETRIES_EXCEEDED_ERROR(HttpStatus.INTERNAL_SERVER_ERROR, -11001, "외부 API 호출 재시도 횟수 초과"),
-    SEARCH_KEYWORD_LENGTH(HttpStatus.BAD_REQUEST, -10000, "검색어는 2자 이상 60자 이하로 입력해주세요."),
+    SEARCH_KEYWORD_LENGTH(HttpStatus.BAD_REQUEST, -10000, "검색어는 1자 이상 60자 이하로 입력해주세요."),
     RADIUS_SIZE_TOO_SMALL(HttpStatus.BAD_REQUEST, -10000, "검색 반경은 500m 이상 4000m 이하로 입력해주세요."),
 }
--- src/main/kotlin/kr/weit/roadyfoody/search/foodSpots/dto/FoodSpotsSearchCondition.kt
+++ src/main/kotlin/kr/weit/roadyfoody/search/foodSpots/dto/FoodSpotsSearchCondition.kt
@@ -2,6 +2,7 @@ package kr.weit.roadyfoody.search.foodSpots.dto
 
 import jakarta.validation.constraints.Min
 import jakarta.validation.constraints.NotNull
+import jakarta.validation.constraints.Size
 import kr.weit.roadyfoody.foodSpots.validator.Latitude
 import kr.weit.roadyfoody.foodSpots.validator.Longitude
 import org.springdoc.core.annotations.ParameterObject
@@ -17,6 +18,7 @@ data class FoodSpotsSearchCondition(
     @field:NotNull(message = "반경은 필수입니다.")
     @field:Min(value = 500, message = "반경은 500 이상이어야 합니다.")
     val radius: Int,
+    @Size(min = 1, max = 60, message = "검색어는 1자 이상 60자 이하로 입력해주세요.")
     val name: String?,
     val categoryIds: List<Long>? = emptyList(),
 )
