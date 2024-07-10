2d70824779e8d2aea230afff5c9ca534bc32c837
jifrozen0110 <answldjs1836@gmail.com>
2024-07-10 11:11:02 +0900

    ROFO-63 custom 함수 등록
    * Custom SQL Function 등록 (현재 작동은 안하지만 공유 차원에서 올림)

---
+++ src/main/kotlin/kr/weit/roadyfoody/common/domain/query/CustomFunctionContributor.kt
@@ -0,0 +1,15 @@
+package kr.weit.roadyfoody.common.domain.query
+
+import org.hibernate.boot.model.FunctionContributions
+import org.hibernate.boot.model.FunctionContributor
+import org.hibernate.dialect.function.StandardSQLFunction
+import org.hibernate.type.StandardBasicTypes
+
+class CustomFunctionContributor : FunctionContributor {
+    override fun contributeFunctions(functionContributions: FunctionContributions) {
+        functionContributions.functionRegistry.register(
+            "myContains",
+            StandardSQLFunction("contains", StandardBasicTypes.BOOLEAN),
+        )
+    }
+}
