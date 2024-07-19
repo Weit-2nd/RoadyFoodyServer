3d34949e69aacf06099c8bfae681f1b5d4a1aba2
jifrozen0110 <answldjs1836@gmail.com>
2024-07-19 23:48:40 +0900

    ROFO-104 분산락 어노테이션 생성

---
+++ src/main/kotlin/kr/weit/roadyfoody/common/annotation/DistributedLock.kt
@@ -0,0 +1,12 @@
+package kr.weit.roadyfoody.common.annotation
+
+import java.util.concurrent.TimeUnit
+
+@Target(AnnotationTarget.FUNCTION)
+@Retention(AnnotationRetention.RUNTIME)
+annotation class DistributedLock(
+    val lockName: String,
+    val waitTime: Long = 10L,
+    val leaseTime: Long = 30L,
+    val timeUnit: TimeUnit = TimeUnit.SECONDS,
+)
