324b322f976fd1c809894f705ae37292d0e04259
jifrozen0110 <answldjs1836@gmail.com>
2024-07-21 21:46:39 +0900

    ROFO-100 분산락 테스트 동시성 환경 코루틴으로 변경

--- src/test/kotlin/kr/weit/roadyfoody/global/aop/DistributedAopTest.kt
+++ src/test/kotlin/kr/weit/roadyfoody/global/aop/DistributedAopTest.kt
@@ -2,11 +2,11 @@ package kr.weit.roadyfoody.global.aop
 
 import io.kotest.core.spec.style.BehaviorSpec
 import io.kotest.matchers.shouldBe
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.runBlocking
 import kr.weit.roadyfoody.global.annotation.DistributedLock
 import kr.weit.roadyfoody.support.annotation.ServiceIntegrateTest
 import org.springframework.stereotype.Service
-import java.util.concurrent.CountDownLatch
-import java.util.concurrent.Executors
 
 @ServiceIntegrateTest
 class DistributedAopTest(
@@ -14,26 +14,26 @@ class DistributedAopTest(
 ) : BehaviorSpec({
         given("코인 감소 기능에서") {
             `when`("동시에 코인을 감소시키면") {
-                val numberOfThreads = 50
+                val numberOfCoroutines = 10
+                val initialCoin = 100
+                val expectedCoin = initialCoin - numberOfCoroutines
 
-                val executor = Executors.newFixedThreadPool(numberOfThreads)
-                val latch = CountDownLatch(numberOfThreads)
-                val initialCoin = coinService.coin
-                val expectedCoin = initialCoin - numberOfThreads
-
-                repeat(numberOfThreads) {
-                    executor.submit {
-                        try {
-                            coinService.decreaseCoin(1L)
-                        } finally {
-                            latch.countDown()
-                        }
-                    }
+                beforeEach {
+                    // 초기 코인 값 설정
+                    coinService.coin = initialCoin
                 }
-                latch.await()
-                executor.shutdown()
                 then("코인이 90개 남는다.") {
-                    coinService.coin shouldBe expectedCoin
+                    runBlocking {
+                        val jobs =
+                            List(numberOfCoroutines) {
+                                launch {
+                                    coinService.decreaseCoin(1L)
+                                }
+                            }
+
+                        jobs.forEach { it.join() }
+                        coinService.coin shouldBe expectedCoin
+                    }
                 }
             }
         }
