9f9c312c83ad91f6647475ca46962ccead051c41
konempty <jameswebb@naver.com>
2024-08-04 18:14:41 +0900

    ROFO-142 주소 API 오류 수정

--- src/main/kotlin/kr/weit/roadyfoody/search/address/dto/Point2AddressResponse.kt
+++ src/main/kotlin/kr/weit/roadyfoody/search/address/dto/Point2AddressResponse.kt
@@ -4,8 +4,8 @@ import io.swagger.v3.oas.annotations.media.Schema
 
 @Schema(description = "도로명 주소 검색 결과")
 data class Point2AddressResponse(
-    @Schema(description = "도로명 주소", example = "경기도 안성시 죽산면 죽산초교길 69-4")
-    val roadAddressName: String,
+    @Schema(description = "도로명 주소(optional)", example = "경기도 안성시 죽산면 죽산초교길 69-4")
+    val roadAddressName: String?,
     @Schema(description = "지번 주소", example = "경기 안성시 죽산면 죽산리 343-1")
     val addressName: String,
     @Schema(description = "위도", example = "37.0789561558879")
@@ -21,7 +21,7 @@ data class Point2AddressResponse(
         ): Point2AddressResponse =
             Point2AddressResponse(
                 addressName = document.address.addressName,
-                roadAddressName = document.roadAddress.addressName,
+                roadAddressName = document.roadAddress?.addressName,
                 latitude = latitude,
                 longitude = longitude,
             )
