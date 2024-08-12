e4dfb09e4ea72182ecc63899dcb1bb0bf42c0393
hyunjungkimm <khjjung1124@gmail.com>
2024-08-12 18:24:23 +0900

    [ROFO-152] on delete set null 설정 food_spots_history_id에 추가

--- src/main/resources/db/migration/V11__alter_add_rewards.sql
+++ src/main/resources/db/migration/V11__alter_add_rewards.sql
@@ -6,3 +6,8 @@ ALTER TABLE REWARDS
     ADD reward_type NUMBER(1) NOT NULL;
 
 ALTER TABLE REWARDS ADD reward_reason VARCHAR2(20) NOT NULL;
+
+ALTER TABLE rewards
+    ADD CONSTRAINT FK_REWARDS_ON_FOOD_SPOTS_HISTORIES
+        FOREIGN KEY (food_spots_history_id) REFERENCES food_spots_histories(id)
+            ON DELETE SET NULL;
