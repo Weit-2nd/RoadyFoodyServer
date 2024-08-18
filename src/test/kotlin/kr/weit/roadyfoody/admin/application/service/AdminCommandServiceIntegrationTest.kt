bf83d7ffafbaa35368994e080dd155de5b165178
unagi-zoso <unagi.zoso@gmail.com>
2024-08-19 04:06:56 +0900

    ROFO-157 유저 일일 리포트 생성 횟수 변경 API 작성

---
+++ src/test/kotlin/kr/weit/roadyfoody/admin/application/service/AdminCommandServiceIntegrationTest.kt
@@ -0,0 +1,29 @@
+package kr.weit.roadyfoody.admin.application.service
+
+import io.kotest.core.spec.style.BehaviorSpec
+import io.kotest.matchers.shouldBe
+import kr.weit.roadyfoody.foodSpots.application.service.FoodSpotsCommandService.Companion.getFoodSpotsReportCountKey
+import kr.weit.roadyfoody.support.annotation.ServiceIntegrateTest
+import kr.weit.roadyfoody.user.fixture.TEST_USER_ID
+import org.springframework.data.redis.core.RedisTemplate
+
+@ServiceIntegrateTest
+class AdminCommandServiceIntegrationTest(
+    private val adminCommandService: AdminCommandService,
+    private val redisTemplate: RedisTemplate<String, String>,
+) : BehaviorSpec({
+        given("이미 생성 횟수가 5인 회원이 있을 때") {
+            val userKey = getFoodSpotsReportCountKey(TEST_USER_ID)
+            redisTemplate.opsForValue().set(userKey, "5")
+
+            `when`("그 회원의 생성 횟수를 1로 변경 요청하면") {
+                adminCommandService.updateUserDailyReportCount(TEST_USER_ID, 1)
+
+                then("redis 에 값이 변경되어야 한다.") {
+                    val restReportCount = redisTemplate.opsForValue().get(userKey)
+                    restReportCount shouldBe "1"
+                }
+            }
+            redisTemplate.delete(userKey)
+        }
+    })
--- src/test/kotlin/kr/weit/roadyfoody/admin/presentation/api/AdminControllerTest.kt
+++ src/test/kotlin/kr/weit/roadyfoody/admin/presentation/api/AdminControllerTest.kt
@@ -2,7 +2,11 @@ package kr.weit.roadyfoody.admin.presentation.api
 
 import com.ninjasquad.springmockk.MockkBean
 import io.kotest.core.spec.style.BehaviorSpec
+import io.kotest.data.forAll
+import io.kotest.data.row
 import io.mockk.every
+import io.mockk.just
+import io.mockk.runs
 import kr.weit.roadyfoody.admin.application.service.AdminCommandService
 import kr.weit.roadyfoody.admin.application.service.AdminQueryService
 import kr.weit.roadyfoody.admin.dto.UserAccessTokenResponse
@@ -13,6 +17,7 @@ import kr.weit.roadyfoody.user.fixture.TEST_USER_ID
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest
 import org.springframework.test.web.servlet.MockMvc
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders.put
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers.status
 
 @WebMvcTest(AdminController::class)
@@ -41,6 +46,59 @@ class AdminControllerTest(
                     mockMvc
                         .perform(get("$requestUrl/users/$TEST_USER_ID/token"))
                         .andExpect(status().isOk)
+                }
+            }
+
+            `when`("userId 가 0이나 음수로 요청하면") {
+                every { adminCommandService.getUserAccessToken(any()) } returns UserAccessTokenResponse(TEST_ACCESS_TOKEN)
+                then("400 상태코드를 반환한다.") {
+                    forAll(
+                        row(0),
+                        row(-1),
+                    ) { userId ->
+                        mockMvc
+                            .perform(get("$requestUrl/users/$userId/token"))
+                            .andExpect(status().isBadRequest)
+                    }
+                }
+            }
+        }
+
+        given("PUT $requestUrl/users/{userId}/daily-report-count 테스트") {
+            val dailyReportCount = 1
+
+            `when`("정상적으로 요청하면") {
+                every { adminCommandService.updateUserDailyReportCount(TEST_USER_ID, any()) } just runs
+                then("204 상태코드를 반환한다.") {
+                    mockMvc
+                        .perform(put("$requestUrl/users/$TEST_USER_ID/daily-report-count?dailyReportCount=$dailyReportCount"))
+                        .andExpect(status().isNoContent)
+                }
+            }
+
+            `when`("userId 가 0이나 음수로 요청하면") {
+                every { adminCommandService.updateUserDailyReportCount(any(), any()) } just runs
+
+                then("400 상태코드를 반환한다.") {
+                    forAll(
+                        row(0),
+                        row(-1),
+                    ) { userId ->
+                        mockMvc
+                            .perform(put("$requestUrl/users/$userId/daily-report-count?dailyReportCount=$dailyReportCount"))
+                            .andExpect(status().isBadRequest)
+                    }
+                }
+            }
+
+            `when`("dailyReportCount 가 음수로 요청하면") {
+                every { adminCommandService.updateUserDailyReportCount(TEST_USER_ID, -1) } just runs
+                val negativeDailyReportCount = -1
+
+                then("400 상태코드를 반환한다.") {
+                    mockMvc
+                        .perform(put("$requestUrl/users/$TEST_USER_ID/daily-report-count?dailyReportCount=$negativeDailyReportCount"))
+                        .andExpect(status().isBadRequest)
                 }
             }
         }
