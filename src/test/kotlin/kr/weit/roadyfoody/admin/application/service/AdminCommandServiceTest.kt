692545001ae75ff5c1c11acd065f6bafff92965f
unagi-zoso <unagi.zoso@gmail.com>
2024-08-19 04:39:41 +0900

    ROFO-157 updateUserDailyReportCount 유지 미존재 시 예외 추가

--- src/test/kotlin/kr/weit/roadyfoody/admin/application/service/AdminCommandServiceTest.kt
+++ src/test/kotlin/kr/weit/roadyfoody/admin/application/service/AdminCommandServiceTest.kt
@@ -1,11 +1,13 @@
 package kr.weit.roadyfoody.admin.application.service
 
+import io.kotest.assertions.throwables.shouldThrow
 import io.kotest.core.spec.style.BehaviorSpec
 import io.kotest.matchers.shouldBe
 import io.mockk.every
 import io.mockk.mockk
 import kr.weit.roadyfoody.auth.fixture.TEST_ACCESS_TOKEN
 import kr.weit.roadyfoody.auth.security.jwt.JwtUtil
+import kr.weit.roadyfoody.user.exception.UserNotFoundException
 import kr.weit.roadyfoody.user.fixture.TEST_USER_ID
 import kr.weit.roadyfoody.user.fixture.createTestUser
 import kr.weit.roadyfoody.user.repository.UserRepository
@@ -26,6 +28,25 @@ class AdminCommandServiceTest :
                 then("UserAccessTokenResponse 를 반환한다.") {
                     val actual = adminCommandService.getUserAccessToken(TEST_USER_ID).accessToken
                     actual shouldBe TEST_ACCESS_TOKEN
+                }
+            }
+        }
+
+        given("updateUserDailyReportCount 테스트") {
+            `when`("유저 ID 와 일일 신고 횟수로 요청하면") {
+                every { userRepository.existsById(any()) } returns true
+                every { redisTemplate.opsForValue().set(any(), any()) } returns Unit
+                then("성공한다.") {
+                    adminCommandService.updateUserDailyReportCount(TEST_USER_ID, 1)
+                }
+            }
+
+            `when`("유저 ID 가 존재하지 않으면") {
+                every { userRepository.existsById(any()) } returns false
+                then("UserNotFoundException 이 발생한다.") {
+                    shouldThrow<UserNotFoundException> {
+                        adminCommandService.updateUserDailyReportCount(TEST_USER_ID, 1)
+                    }
                 }
             }
         }
